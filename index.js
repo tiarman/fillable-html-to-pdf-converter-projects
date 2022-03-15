aae4f4e322a7cd9e1a8e5f6c247204c115a479c7
tiarman <tiarmancr@gmail.com>
2022-03-15 19:44:23 +0600

    completed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ window.onload = function () {
                 filename: 'myfile.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+                jsPDF: { unit: 'in', format: 'A4', orientation: 'portrait' }
             };
             html2pdf().from(invoice).set(opt).save();
         })
